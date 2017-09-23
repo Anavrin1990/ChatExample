1d888e41f22ce1158f87c24a46b072e3f8e2f252
Dmitry <DmitryMac@iMac-Dmitry.local>
2017-09-24 02:22:40 +0500

    first

---
+++ Chat/BubbleCollectionViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  BubbleCollectionViewCell.swift
+//  Chat
+//
+//  Created by Dmitry on 23.09.17.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class BubbleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!    
+    @IBOutlet weak var textView: UITextView! 
+    @IBOutlet weak var bubbleView: UIView! {
+        didSet {
+            bubbleView.layer.masksToBounds = true
+            bubbleView.layer.cornerRadius = 15
+        }
+    }
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    
+    // Заполняем ячейку
+    func fillCell(_ message: Message) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        let date = dateFormatter.date(from: message.date ?? "")
+        dateFormatter.dateFormat = "HH:mm"
+        if let date = date {
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        nameLabel.text = message.user
+        textView.text = message.text
+        
+        if let isMine = message.isMine {
+            leadingConstraint?.isActive = !isMine
+            trailingConstraint?.isActive = isMine
+            
+            let bubbleColor = isMine ? #colorLiteral(red: 0.2784313725, green: 0.7411764706, blue: 0.3411764706, alpha: 1) : #colorLiteral(red: 0.8978190422, green: 0.8985271454, blue: 0.9185681939, alpha: 1)
+            let textColor = isMine ? UIColor.white : UIColor.darkText
+            let headerTextColor = isMine ? #colorLiteral(red: 0.6841868162, green: 0.8855811954, blue: 0.7207990289, alpha: 1) : #colorLiteral(red: 0.6117165089, green: 0.6117919087, blue: 0.6116907597, alpha: 1)
+            
+            dateLabel.textColor = headerTextColor
+            nameLabel.textColor = headerTextColor
+            textView.textColor = textColor
+            bubbleView.backgroundColor = bubbleColor
+        }
+        
+    }
+}
---
+++ Chat/ChatViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ChatViewController.swift
+//  Chat
+//
+//  Created by Dmitry on 23.09.17.
+//  Copyright © 2017 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+struct jsDecoder: Decodable {
+    let messages: [Message]
+}
+
+struct Message: Decodable {
+    var text: String?
+    var date: String?
+    var isMine: Bool?
+    var user: String?
+}
+
+class ChatViewController: UIViewController {
+    
+    var messages = [Message]()    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Парсим джейсон свифтом 4
+        let path = Bundle.main.path(forResource: "messages", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        let data = try! Data(contentsOf: url)
+        
+        do {
+            let decodedData = try JSONDecoder().decode(jsDecoder.self, from: data)
+            messages = decodedData.messages
+        } catch let error {
+            print(error)
+        }
+        
+        let bubbleCell = UINib(nibName: "BubbleCollectionViewCell", bundle: nil)
+        collectionView.register(bubbleCell, forCellWithReuseIdentifier: "Cell")
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Сроллим вниз
+        let indexPath = IndexPath(item: messages.count - 1, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .top, animated: true)
+    }
+    
+}
+
+extension ChatViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! BubbleCollectionViewCell
+        let message = messages[indexPath.row]
+        cell.fillCell(message)
+        
+        if let frame = getEstimatedFrame(text: message.text) {
+            let width = frame.width + 40
+            cell.widthConstraint.constant = width < 250 ? 250 : width
+        }
+        
+        return cell
+    }
+    
+    // Получаем размер текста
+    func getEstimatedFrame(text: String?) -> CGRect? {
+        guard let text = text else {return nil}
+        let size = CGSize(width: 300, height: 1000)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        let estimatedFrame = NSString(string: text).boundingRect(with: size, options: options, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 18)], context: nil)
+        
+        return estimatedFrame
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var height: CGFloat = 0
+        
+        if let frame = getEstimatedFrame(text: messages[indexPath.row].text) {
+            height = frame.height + 45
+        }
+        
+        return CGSize(width: collectionView.frame.width, height: height)
+    }
+    
+}
